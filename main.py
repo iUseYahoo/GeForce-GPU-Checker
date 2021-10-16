68f5dfb0e20a278f9724fbb5593465e30d7c204b
iUseYahoo <logicatedlzyt@gmail.com>
2021-10-17 02:15:10 +1100

    Fixed repo, Wasnt working after previous push.

--- main.py
+++ main.py
@@ -1,6 +1,55 @@
-import Checkers.GPU3090 as check3090
-import Checkers.GPU3080 as check3080
-import Checkers.GPU3070 as check3070
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+
+checkTime = 8
+
+gpu3090store = "https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3090" # url of the store, no touchy
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
+GPU3090name = "GeForce RTX 3090"
+
+gpu3080store = "https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3080" # url of the store, no touchy
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
+GPU3080name = "GeForce RTX 3080"
+
+gpu3070store = "https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3070" # url of the store, no touchy
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
+GPU3070name = "GeForce RTX 3070"
+
+class CheckGPUs:
+    def gpu3090():
+        site = requests.get(gpu3090store, headers=headers)
+        soup = BeautifulSoup(site.content, 'html.parser')
+
+        getGPUPrice = soup.find(id='ctl00_LFrame_prdList_rlvProdList_ctrl0_ctrl1_spanFinalPrice').get_text()
+        print(f"[*] {GPU3090name} Current Price: {getGPUPrice}")
+
+        if (getGPUPrice != "$1739.99"):
+            print(f"[+] GeForce RTX 3090 Price: $1739.99 : {getGPUPrice} | https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3090")
+        sleep(checkTime)
+
+    def gpu3080():
+        site = requests.get(gpu3080store, headers=headers)
+        soup = BeautifulSoup(site.content, 'html.parser')
+
+        getGPUPrice = soup.find(id='ctl00_LFrame_prdList_rlvProdList_ctrl0_ctrl1_spanFinalPrice').get_text()
+        print(f"[*] {GPU3080name} Current Price: {getGPUPrice}")
+
+        if (getGPUPrice != "$899.99"):
+            print(f"[+] GeForce RTX 3080 Price: $899.99 : {getGPUPrice} | {gpu3080store}")
+        sleep(checkTime)
+
+    def gpu3070():
+        site = requests.get(gpu3070store, headers=headers)
+        soup = BeautifulSoup(site.content, 'html.parser')
+
+        getGPUPrice = soup.find(id='ctl00_LFrame_prdList_rlvProdList_ctrl0_ctrl1_spanFinalPrice').get_text()
+        print(f"[*] {GPU3070name} Current Price: {getGPUPrice}")
+
+        if (getGPUPrice != "$709.99"):
+            print(f"[+] GeForce RTX 3070 Price: $709.99 : {getGPUPrice} | {gpu3070store}")
+        sleep(checkTime)
+
 
 def PickGPUChecker():
     print("1 - 3090 (EVGA) | 2 - 3080 (EVGA) | 3 - 3070 (EVGA)")
@@ -18,16 +67,16 @@ def PickGPUChecker():
 def Check3090Evga():
     print("\n")
     while True:
-        check3090
+        CheckGPUs.gpu3090()
 
 def Check3080Evga():
     print("\n")
     while True:
-        check3080
+        CheckGPUs.gpu3080()
         
 def Check3070Evga():
     print("\n")
     while True:
-        check3070
+        CheckGPUs.gpu3070()
 
 PickGPUChecker()
