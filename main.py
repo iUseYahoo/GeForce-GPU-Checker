6d3db5c1285258a9467facbc41ebdb24211a8fe8
iUseYahoo <logicatedlzyt@gmail.com>
2021-10-16 17:21:40 +1100

    Added GPU 3070 Checker

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from time import sleep
 
 gpu3090store = "https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3090" # url of the store, no touchy
 gpu3080store = "https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3080"
+gpu3070store = "https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3070"
 checkTime = 8 # this is the refresh time, pretty much how long for the next check message status
 # You can change the header to a user agent that you want
 # or you can keep it default, it doesnt really matter
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
 GPU3090name = "GeForce RTX 3090"
 GPU3080name = "GeForce RTX 3080"
+GPU3070name = "GeForce RTX 3070"
 
 class CheckGPUs:
     def gpu3090():
@@ -34,15 +36,28 @@ class CheckGPUs:
             print(f"[+] GeForce RTX 3080 Price: $899.99 : {getGPUPrice} | https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3080")
         sleep(checkTime)
 
+    def gpu3070():
+        site = requests.get(gpu3070store, headers=headers)
+        soup = BeautifulSoup(site.content, 'html.parser')
+
+        getGPUPrice = soup.find(id='ctl00_LFrame_prdList_rlvProdList_ctrl0_ctrl1_spanFinalPrice').get_text()
+        print(f"[*] {GPU3070name} Current Price: {getGPUPrice}")
+
+        if (getGPUPrice != "$709.99"):
+            print(f"[+] GeForce RTX 3070 Price: $709.99 : {getGPUPrice} | https://www.evga.com/products/ProductList.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3070")
+        sleep(checkTime)
+
 
 def PickGPUChecker():
-    print("1 - 3090 (EVGA) | 2 - 3080 (EVGA)")
+    print("1 - 3090 (EVGA) | 2 - 3080 (EVGA) | 3 - 3070 (EVGA)")
     pickGPU = input("Pick GPU: ")
 
     if (pickGPU == "1"):
         Check3090Evga()
     elif (pickGPU == "2"):
         Check3080Evga()
+    elif (pickGPU == "3"):
+        Check3070Evga()
     else:
         quit()
 
@@ -55,5 +70,10 @@ def Check3080Evga():
     print("\n")
     while True:
         CheckGPUs.gpu3080()
+        
+def Check3070Evga():
+    print("\n")
+    while True:
+        CheckGPUs.gpu3070()
 
 PickGPUChecker()
